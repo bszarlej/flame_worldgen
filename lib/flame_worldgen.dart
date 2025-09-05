740f4ed4fd7233f9ff8849b141594123d9871662
Bartłomiej Bogdan Szarlej <126085810+bszarlej@users.noreply.github.com>
2025-09-05 11:44:52 +0200

    chore: rename `procedural_world_2d.dart` to `procedurally_generated_world.dart` (#17)
    Co-authored-by: Bartlomiej Bogdan Szarlej <bartlomiej-bogdan.szarlej@dolphin-it.de>

--- lib/flame_worldgen.dart
+++ lib/flame_worldgen.dart
@@ -2,6 +2,6 @@ export 'package:fast_noise/fast_noise.dart';
 
 export 'src/core/chunk.dart';
 export 'src/core/chunk_manager.dart';
-export 'src/core/procedural_world_2d.dart';
+export 'src/core/procedurally_generated_world.dart';
 export 'src/math/vector2i.dart';
 export 'src/utils/utils.dart';

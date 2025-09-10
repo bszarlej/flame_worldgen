eb34b3dc1bddc84ec85a8102f133369f5fd5ac8d
bszarlej <bartlomiejbogdan.dev@gmail.com>
2025-09-10 16:45:32 +0200

    feat: replace `ProcedurallyGeneratedWorld` with a tile layer system

---
+++ lib/src/components/static_tile_layer.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'tile_layer.dart';
+
+class StaticTileLayer extends TileLayer {
+  final Rect? Function(double noiseValue) spriteSelector;
+
+  StaticTileLayer({
+    required super.chunkManager,
+    required super.spriteBatch,
+    required this.spriteSelector,
+    super.centerPositionProvider,
+    super.children,
+    super.key,
+    super.priority,
+  });
+
+  @override
+  Rect? selectSprite(double noiseValue, [int frame = 0]) {
+    return spriteSelector(noiseValue);
+  }
+}
